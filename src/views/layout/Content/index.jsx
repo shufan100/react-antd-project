edd6acfecba24243b85b86f3af0517c7e4e0af1e
shufan100 <1549248097@qq.com>
2022-03-26 20:22:17 +0800

    登录

--- src/views/layout/Content/index.jsx
+++ src/views/layout/Content/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, Suspense, useEffect } from 'react';
-import { Route, Redirect, Switch, useHistory } from 'react-router-dom';
+import { Route, Redirect, Switch, useHistory, withRouter } from 'react-router-dom';
 import { Layout } from 'antd';
 import './index.less';
 import routeMap from '@/config/routeMap';
@@ -7,25 +7,25 @@ import Loading from '@/views/loading';
 const { Content } = Layout;;
 
 // 这里注册路由（react-router-dom)5.0版本为例：
-const LayoutContent = () => {
+const LayoutContent = props => {
   const [routeName, setRouteName] = useState('/home');
   const history = useHistory();
   useEffect(() => {
-    setRouteName(history.location.pathname);
+    // setRouteName(history.location.pathname);
   }, []);
 
   return (
     <Content className='Content' style={{ height: 'calc(100% - 100px)' }}>
-      <Switch>
+      <Switch location={props.location.pathname}>
         <Suspense fallback={<Loading />}>
           <Redirect exact from="/" to="/home" />
           {
             routeMap.map(item => <Route key={item.path} path={item.path} component={item.component} />)
           }
-          <Redirect exact to={routeName} />
+          {/* <Redirect exact to='/home' /> */}
         </Suspense>
       </Switch>
     </Content >
   );
 };
-export default LayoutContent;
+export default withRouter(LayoutContent);
