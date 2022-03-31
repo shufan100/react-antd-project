ccfb65fa1ec7926d1400c865ff455d471295858c
shufan100 <1549248097@qq.com>
2022-03-31 17:03:13 +0800

    axios封装

---
+++ src/utils/request.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import store from '@/store';
+import { Modal } from 'antd';
+
+//创建一个axios示例
+const service = axios.create({
+  baseURL: '/api1', // api 的 base_url
+  timeout: 50000, // request timeout
+});
+
+// 请求拦截器
+service.interceptors.request.use(config => {
+  console.log('请求拦截器>>>', config);
+
+  // 根据接口修改代理
+  if (config.url.indexOf('cars') !== -1) {
+    config.baseURL = '/api2';
+  } else {
+    config.baseURL = '/api1';
+  }
+
+  // 让每个请求携带token--
+  if (store.getState().user.token) {
+    config.headers.token = store.getState().user.token;
+  }
+
+  return config;
+},
+  // 发送请求错误：(创建失败Promise对象并将error数据返回)
+  error => Promise.reject(error)
+);
+
+// 响应拦截器
+service.interceptors.response.use(
+  // 请求返回成功
+  response => {
+    const res = response.data;
+    // console.log(res, '--');
+    // if (res.code !== 50000) {
+    //   //     // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
+    //   //     if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+    //   //       // 请自行在引入 MessageBox
+    //   //       // import { Message, MessageBox } from 'element-ui'
+    //   //       MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
+    //   //         confirmButtonText: '重新登录',
+    //   //         cancelButtonText: '取消',
+    //   //         type: 'warning'
+    //   //       }).then(() => {
+    //   //         store.dispatch('FedLogOut').then(() => {
+    //   //           location.reload() // 为了重新实例化vue-router对象 避免bug
+    //   //         })
+    //   //       })
+    //   //     }
+    //   return Promise.reject('接口状态不为200！');
+    // } else {
+    return response.data;
+    // }
+  },
+  //请求返回失败：(创建失败Promise对象并将error数据返回)
+  error => Promise.reject(error)
+);
+
+export default service;
--- src/views/page/Home/index.jsx
+++ src/views/page/Home/index.jsx
@@ -1,16 +1,41 @@
 import React, { useState, useEffect } from 'react';
-
+import { getUser1, getUser2, getCars } from '@/api';
+import axios from 'axios';
 export default function Home () {
-  const [states, setStates] = useState({});
+  const [states, setStates] = useState({
+    list: []
+  });
   useEffect(() =>
     // console.log('组件已经挂载');
     () => {
       console.log('卸载组件前');
     }
     , []);
+
+  const getDatas = async () => {
+    const result = await axios.get('https://api.apiopen.top/getJoke');
+    console.log(result);
+  };
+  const getNodeUser1 = async () => {
+    const result = await getUser1(1);
+    console.log(result);
+
+  };
+  const getNodeUser2 = async () => {
+    const result = await getUser2();
+    console.log(result);
+  };
+  const getNode2Cars = async () => {
+    const result = await getCars();
+    console.log(result);
+  };
   return (
     <div>
       <div>首页页面</div>
+      <button onClick={getDatas}>axios1</button>
+      <button onClick={getNodeUser1}>node1--5000</button>
+      <button onClick={getNodeUser2}>node2--5000</button>
+      <button onClick={getNode2Cars}>node--5001</button>
     </div>
   );
 }
