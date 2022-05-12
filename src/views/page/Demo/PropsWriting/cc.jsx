e3211df73614ea79c1b8996534c7d1575c261165
shufan100 <1549248097@qq.com>
2022-05-12 18:27:47 +0800

    props传参+父组件ref调用子组件方法

--- src/views/page/Demo/PropsWriting/cc.jsx
+++ src/views/page/Demo/PropsWriting/cc.jsx
@@ -1,16 +1,16 @@
 import React, { forwardRef, useImperativeHandle } from 'react'
 
 const ChildCom = (props, ref) => {
+  // 将子组件的方法暴露出去，让父级通过ref能获取到里面的方法
   useImperativeHandle(ref, () => ({
     getData
   }))
   const getData = () => {
-    // to do something
     alert(666)
   }
   return (
     <div>⼦组件</div>
   )
 }
-
+// forwardRef渲染函数不支持propTypes或defaultProps
 export default forwardRef(ChildCom)
