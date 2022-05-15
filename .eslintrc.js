009258042462f6b25c3618fa7890f7250c547ccd
shufan100 <1549248097@qq.com>
2022-05-15 17:26:46 +0800

    代码格式化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,62 @@
 // 修改需要重启项目
 module.exports = {
   // 环境，这里可以设置环来做区别判断
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es6': true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
   },
   // 使用的扩展库
   // extends: ['airbnb'],
-  'extends': ['react-app', 'plugin:jsx-a11y/recommended'],
+  extends: ['react-app', 'plugin:jsx-a11y/recommended'],
   // 解析器用于解析代码
-  'parser': 'babel-eslint',
+  parser: 'babel-eslint',
   // 解析器配置
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
   // 可以全局使用变量
-  'globals': {
-    'Babel': true,
+  globals: {
+    Babel: true,
 
-    'React': true
+    React: true,
   },
   // 第三方插件
-  'plugins': ['react', 'jsx-a11y'],
+  plugins: ['react', 'jsx-a11y'],
   /**规则配置
-  * "off"或者0    //关闭规则关闭
-  * "warn"或者1    //在打开的规则作为警告（不影响退出代码）
-  * "error"或者2    //把规则作为一个错误（退出代码触发时为1）
+   * "off"或者0    //关闭规则关闭
+   * "warn"或者1    //在打开的规则作为警告（不影响退出代码）
+   * "error"或者2    //把规则作为一个错误（退出代码触发时为1）
    */
-  'rules': {
-    'strict': 2,//使用严格模式
+  rules: {
+    strict: 2, //使用严格模式
     'linebreak-style': [0, 'unix'], //换行风格
-    'quotes': ['error', 'single'], //单双引号
-    'quote-props': 0,//对象字面量中的属性名是否强制双引号
+    quotes: ['error', 'single'], //单双引号
+    'quote-props': 0, //对象字面量中的属性名是否强制双引号
     // 'semi': ['error', 'always'], //分号
-    'semi': 0, //分号
+    semi: 0, //分号
     'no-console': 0, //禁止使用console
-    'indent': 0, // 禁止缩进错误
-    'no-redeclare': 2,//禁止重复声明变量
-    'no-extra-parens': 2,//禁止非必要的括号
-    'no-extra-semi': 2,//禁止多余的冒号
-    'no-func-assign': 2,//禁止重复的函数声明
-    'no-unreachable': 2,//不能有无法执行的代码
-    'no-use-before-define': 2,//未定义前不能使用
-    'no-var': 0,            //禁用var，用let和const代替
-    'arrow-body-style': [2, 'as-needed'],  // 箭头函数直接返回的时候不需要 大括号 {}
+    indent: 0, // 禁止缩进错误
+    'no-redeclare': 2, //禁止重复声明变量
+    'no-extra-parens': 0, //禁止非必要的括号
+    'no-extra-semi': 2, //禁止多余的冒号
+    'no-func-assign': 2, //禁止重复的函数声明
+    'no-unreachable': 2, //不能有无法执行的代码
+    'no-use-before-define': 2, //未定义前不能使用
+    'no-var': 0, //禁用var，用let和const代替
+    'arrow-body-style': [2, 'as-needed'], // 箭头函数直接返回的时候不需要 大括号 {}
     'no-restricted-syntax': 0, // 允许使用 for in
-    'no-trailing-spaces': 0,// 允许代码后面空白
+    'no-trailing-spaces': 0, // 允许代码后面空白
     'no-return-await': 0, // 允许返回 await
     'consistent-return': 0, // 不需要每次都有返回
-    'prefer-rest-params': 0,  // 允许使用 arguments
-    'comma-dangle': ['error', 'never'],//对象字面量项尾不能有逗号
+    'prefer-rest-params': 0, // 允许使用 arguments
+    'comma-dangle': ['error', 'never'], //对象字面量项尾不能有逗号
     'no-underscore-dangle': 0, // 标识符不能以_开头或结尾
-    'no-alert': 0,//禁止使用alert confirm prompt
+    'no-alert': 0, //禁止使用alert confirm prompt
     'no-param-reassign': 0, //禁止给参数重新赋值
     'guard-for-in': 0, //for in循环要用if语句过滤
 
@@ -71,11 +71,10 @@ module.exports = {
     // 引用时候根据根目录基础
     'import/no-unresolved': 0,
     // 允许在 .js 和 .jsx 文件中使用  jsx
-    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     // jsx > 紧跟着属性
     // 'react/jsx-closing-bracket-location': [1, 'after-props'],
     // 不区分是否是 无状态组件
-    'react/prefer-stateless-function': 0
-
-  }
-};
+    'react/prefer-stateless-function': 0,
+  },
+}
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,40 +1,36 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  HashRouter,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from 'react'
+import { BrowserRouter, HashRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 // import { getUserInfo } from '@/store/actions';
-import Login from '../views/login';
-import Layout from '../views/layout';
-
+import Login from '../views/login'
+import Layout from '../views/layout'
 
 class Router extends React.Component {
-  render () {
-    const { token } = this.props;
-    console.log(token, '---');
+  render() {
+    const { token } = this.props
+    console.log(token, '---')
 
     return (
       // 路由一定要用这个容器包裹HashRouter 或 BrowserRouter
       // HashRouter刷新浏览器，路由使用state传参的话，会丢失state路由数据
-      <BrowserRouter hashType="slash">
+      <BrowserRouter hashType='slash'>
         <Switch>
           {/* exact精准匹配 */}
-          <Route exact path="/login" component={Login} />
-          <Route path="/" render={() => {
-            if (!token) {
-              return <Redirect to="/login" />;
-            } else {
-              return <Layout />;
-            }
-          }} />
+          <Route exact path='/login' component={Login} />
+          <Route
+            path='/'
+            render={() => {
+              if (!token) {
+                return <Redirect to='/login' />
+              } else {
+                return <Layout />
+              }
+            }}
+          />
         </Switch>
       </BrowserRouter>
-    );
+    )
   }
 }
 
-export default connect(state => state.user)(Router);
+export default connect(state => state.user)(Router)
